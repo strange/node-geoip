4b4463ad8ea54e1280074c3dac8fc3b1d2a34b2a
Johan Dahlberg <dahlberg.johan@gmail.com>
2011-01-28 12:41:34 +0100

    `Nodified ` javascript connection a little bit

--- examples/lookup2.js
+++ examples/lookup2.js
@@ -1,15 +1,21 @@
-var sys = require('sys'),
-    geoip = require('../lib/geoip');
+var geoip = require('../lib/geoip');
 
 var dbpath = '/usr/local/share/GeoIP/GeoLiteCity.dat';
 var ip = '216.236.135.152';
 
-sys.puts('Looking up ip: ' + ip + '...\n');
-var con = new geoip.Connection(dbpath, function(con) {
-  con.query(ip, function(result) {
-    for (var attr in result) {
-      sys.puts(attr + ' : ' + result[attr]);
-    }
-    con.close();
-  });
+console.log('Looking up ip: ' + ip + '...\n');
+
+var con = geoip.createConnection();
+
+con.on("error", function(err) {
+  console.log(err);
 });
+
+con.query(ip, function(result) {
+  for (var attr in result) {
+    console.log(attr + ' : ' + result[attr]);
+  }
+  con.close();
+});
+
+con.connect(dbpath);
